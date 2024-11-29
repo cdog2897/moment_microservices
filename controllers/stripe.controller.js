72f81c382dad875b585bd7e6bb68e2cba34d0257
cdog2897 <caleb.wolin@gmail.com>
2024-11-28 22:04:41 -0700

    logs

--- controllers/stripe.controller.js
+++ controllers/stripe.controller.js
@@ -1,22 +1,24 @@
 
-// import Stripe from "stripe";
+import Stripe from "stripe";
 
-// const stripe = new Stripe('pk_test_51Kv9OAJHkFGBiS12hWn7RCQA1s8SnNTnfTirggYAvlQmYqYQMXizBIKE4KVliNZor8sz9EmIgkDba948MwKgDORS00zU6OmZFA');
+const stripe = new Stripe('pk_test_51Kv9OAJHkFGBiS12hWn7RCQA1s8SnNTnfTirggYAvlQmYqYQMXizBIKE4KVliNZor8sz9EmIgkDba948MwKgDORS00zU6OmZFA');
 
 export const postPaymentIntent = async (req, res) => {
 
     try {
-        // const paymentIntent = await stripe.paymentIntents.create({
-        //     amount: 1099,
-        //     currency: 'usd'
-        // });
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 1099,
+            currency: 'usd'
+        })
+
+        req.log.info("Received payment intent: ", paymentIntent.client_secret)
 
         res.json({
-            // paymentIntent: paymentIntent.client_secret
+            paymentIntent: paymentIntent.client_secret,
             "caleb wolin" : "rocks"
         })
     } catch (error) {
-        req.log.error("Error creating payment intent: ", error)
+        req.log.error("Error creating payment intent: ", error.message)
         res.status(500).json({ error: 'Internal Server Error' });
       }
 }
