83e2be1588a7ea25c6258b4f213996bedc86382d
cdog2897 <caleb.wolin@gmail.com>
2024-11-28 21:55:20 -0700

    testing

--- controllers/stripe.controller.js
+++ controllers/stripe.controller.js
@@ -1,18 +1,19 @@
 
-import Stripe from "stripe";
+// import Stripe from "stripe";
 
-const stripe = new Stripe('pk_test_51Kv9OAJHkFGBiS12hWn7RCQA1s8SnNTnfTirggYAvlQmYqYQMXizBIKE4KVliNZor8sz9EmIgkDba948MwKgDORS00zU6OmZFA');
+// const stripe = new Stripe('pk_test_51Kv9OAJHkFGBiS12hWn7RCQA1s8SnNTnfTirggYAvlQmYqYQMXizBIKE4KVliNZor8sz9EmIgkDba948MwKgDORS00zU6OmZFA');
 
 export const postPaymentIntent = async (req, res) => {
 
     try {
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: 1099,
-            currency: 'usd'
-        });
+        // const paymentIntent = await stripe.paymentIntents.create({
+        //     amount: 1099,
+        //     currency: 'usd'
+        // });
 
         res.json({
-            paymentIntent: paymentIntent.client_secret
+            // paymentIntent: paymentIntent.client_secret
+            "caleb wolin" : "rocks"
         })
     } catch (error) {
         req.log.error("Error creating payment intent: ", error)
