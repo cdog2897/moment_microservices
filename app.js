e49170a8434cf781a5b4c5086d1011a299a5dd32
cdog2897 <caleb.wolin@gmail.com>
2024-11-28 19:05:55 -0700

    update logs

--- app.js
+++ app.js
@@ -22,11 +22,23 @@ app.use(pinoHttp);
 
 // Example endpoint
 app.get('/', async (req, res) => {
+
   // Use basic logger without HTTP request info
   logger.info({logField: 'custom-entry', arbitraryField: 'custom-entry'}); // Example of structured logging
+
+
   // Use request-based logger with log correlation
-  req.log.info('Child logger with trace Id.'); // https://cloud.google.com/run/docs/logging#correlate-logs
+  req.log.info('Child logger with trace Id.');
+  
+  req.log.info('another example of a log.');
+
+  req.log.error("ERROR MESSAGE!!!")
+
+  req.log.warn("WARNING MESAGe")
+
   res.send('Hello World!');
+
+
 });
 
 export default app;
