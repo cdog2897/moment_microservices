0bfcfb99f4a7d0202e63b6c82cf7469e82a7652f
cdog2897 <caleb.wolin@gmail.com>
2024-11-29 18:22:21 -0700

    logging fix

--- controllers/stripe.controller.js
+++ controllers/stripe.controller.js
@@ -7,7 +7,7 @@ export const postPaymentIntent = async (req, res) => {
 
     const { amount, currency, shipping } = req.body;
 
-    req.log.info("Request received: ", req.body)
+    req.log.info(`Request received: ${req.body}`)
 
     if (!amount || !currency || !shipping) {
         return res.status(400).json({ error: 'Invalid request body' });
@@ -16,14 +16,14 @@ export const postPaymentIntent = async (req, res) => {
     try {
         const paymentIntent = await stripe.paymentIntents.create(req.body)
 
-        req.log.info("Received payment intent: ", paymentIntent.client_secret)
+        req.log.info(`Received payment intent: ${paymentIntent.client_secret}`)
 
         res.json({
             "clientSecret": paymentIntent.client_secret,
             "publishableKey" : "pk_test_51Kv9OAJHkFGBiS12hWn7RCQA1s8SnNTnfTirggYAvlQmYqYQMXizBIKE4KVliNZor8sz9EmIgkDba948MwKgDORS00zU6OmZFA"
         })
     } catch (error) {
-        req.log.error("Error creating payment intent: ", error.message)
+        req.log.error(`Error creating payment intent: ${error.message}`,)
         res.status(500).json({ error: error.message });
       }
 }
