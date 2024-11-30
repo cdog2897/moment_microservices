24426b87bd92fe467052a490b5bd031c6ef54874
cdog2897 <caleb.wolin@gmail.com>
2024-11-29 20:22:15 -0700

    order prodigi validation

--- controllers/prodigi.controller.js
+++ controllers/prodigi.controller.js
@@ -14,31 +14,44 @@ export const submitOrder = async (req, res) => {
         "recipient": {
             "address": {
                 "line1": shipping.address.line1,
-                "line2": shipping.address.line2 ?? "",
+                ...(shipping.address.line2 ? { "line2": shipping.address.line2 } : {}),
                 "postalOrZipCode": shipping.address.postal_code,
                 "countryCode": shipping.address.country,
                 "townOrCity": shipping.address.city,
                 "stateOrCounty": shipping.address.state
             },
             "name": shipping.name,
-            "phoneNumber" : shipping.phone ?? ""
+            ...(shipping.phone ? { "phoneNumber": shipping.phone } : {})
         },
         "items": [
             {
                 "sku": "GLOBAL-PHO-4x6",
                 "copies": 1,
                 "sizing": "fillPrintArea",
+                "attributes" : {
+                    "finish" : "gloss"
+                },
                 "assets": [
-                    {
-                        "printArea": "default",
-                        "url": "https://firebasestorage.googleapis.com:443/v0/b/moment-3c911.appspot.com/o/orders%2FC1321580-615C-4443-A6DB-29C98CA41FF3.jpeg?alt=media&token=f8ad25d6-9c8e-4b14-b55c-31e8fc5a0ea1"
-                    },
                     {
                         "printArea": "default",
                         "url": "https://firebasestorage.googleapis.com:443/v0/b/moment-3c911.appspot.com/o/orders%2FC1F689D3-6E4C-40B7-B1F8-4F609F9F3F3E.jpeg?alt=media&token=2c97ad3a-d938-4c95-b149-2b074b07cc33"
                     }
                 ]
-            }
+            },
+            {
+                "sku": "GLOBAL-PHO-4x6",
+                "copies": 1,
+                "sizing": "fillPrintArea",
+                "attributes" : {
+                    "finish" : "gloss"
+                },
+                "assets": [
+                    {
+                        "printArea": "default",
+                        "url": "https://firebasestorage.googleapis.com:443/v0/b/moment-3c911.appspot.com/o/orders%2FC1321580-615C-4443-A6DB-29C98CA41FF3.jpeg?alt=media&token=f8ad25d6-9c8e-4b14-b55c-31e8fc5a0ea1"
+                    }
+                ]
+            },
         ]
     }
 
@@ -56,7 +69,7 @@ export const submitOrder = async (req, res) => {
         req.log.info(`Response received from Prodigi: ${response.data}`)
         res.status(200).json(response.data)
     } catch (error) {
-        req.log.error(`An error occurred while sending Order request: ${error.message}`)
+        req.log.error(`An error occurred while sending Order request: ${error}`)
         res.status(500).json({"An error occured" : error.message})
     }
     
